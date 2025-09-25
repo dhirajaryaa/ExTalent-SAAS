931a5c0df469135c3c047b16ec21520caa5f65be
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-25 16:13:40 +0000

    feat(server): Add environment variables for CORS and NODE_ENV; implement error middleware

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -4,6 +4,7 @@ const _config = {
   mongo_url: process.env.MONGO_URL || "mongodb://localhost:27017",
   client_url: process.env.CLIENT_URL,
   server_url: process.env.SERVER_URL,
+  node_env: process.env.NODE_ENV,
   //   jwt
   jwt_secret: process.env.JWT_SECRET,
   jwt_expires_in: process.env.JWT_EXPIRES_IN || "1d",
@@ -13,6 +14,8 @@ const _config = {
   github_client_id: process.env.GITHUB_CLIENT_ID,
   github_client_secret: process.env.GITHUB_CLIENT_SECRET,
   github_oauth_redirect_url: process.env.GITHUB_OAUTH_REDIRECT_URL,
+  // cors 
+  origin: process.env.ORIGIN
 };
 
 export const {
@@ -20,6 +23,7 @@ export const {
   mongo_url,
   client_url,
   server_url,
+  node_env,
   jwt_secret,
   jwt_expires_in,
   jwt_refresh_expires_in,
@@ -27,4 +31,5 @@ export const {
   github_client_id,
   github_client_secret,
   github_oauth_redirect_url,
+  origin
 } = _config;
