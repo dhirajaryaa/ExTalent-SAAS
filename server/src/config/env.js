99e0708099343d3ca502beecdeeb8469273bb8be
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-25 16:01:52 +0000

    feat(server): Add environment configuration files and update Prettier settings

---
+++ server/src/config/env.js
@@ -0,0 +1,30 @@
+const _config = {
+  // server
+  port: process.env.PORT || 3000,
+  mongo_url: process.env.MONGO_URL || "mongodb://localhost:27017",
+  client_url: process.env.CLIENT_URL,
+  server_url: process.env.SERVER_URL,
+  //   jwt
+  jwt_secret: process.env.JWT_SECRET,
+  jwt_expires_in: process.env.JWT_EXPIRES_IN || "1d",
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  //   github
+  github_client_id: process.env.GITHUB_CLIENT_ID,
+  github_client_secret: process.env.GITHUB_CLIENT_SECRET,
+  github_oauth_redirect_url: process.env.GITHUB_OAUTH_REDIRECT_URL,
+};
+
+export const {
+  port,
+  mongo_url,
+  client_url,
+  server_url,
+  jwt_secret,
+  jwt_expires_in,
+  jwt_refresh_expires_in,
+  jwt_refresh_secret,
+  github_client_id,
+  github_client_secret,
+  github_oauth_redirect_url,
+} = _config;
