08b9dec46b72551159c08e8a6d7db96cf55740f0
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-26 00:26:04 +0000

    feat(server): auth checker middleware

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import apiError from "../utils/apiError.js";
+import jwt from "jsonwebtoken";
+import { jwt_secret } from "../config/env.js";
+
+const authChecker = asyncHandler(async (req, res, next) => {
+  // get token
+  const incomingToken = req.cookie.accessToken || req.headers.Authorization?.replace("Bearer ", "");
+  if (!incomingToken) {
+    throw new apiError(
+      401,
+      "unAuthorized Request! , please login first and try again!",
+      "AUTHERROR"
+    );
+  }
+
+  //   decode token
+  const decodedToken = jwt.verify(incomingToken, jwt_secret);
+  req.user = { _id: decodedToken._id, name: decodedToken.name };
+  next();
+});
+
+export default authChecker;
