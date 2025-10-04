520c9f38766a7c1053ae29f8312ba3c535a362b0
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-04 23:28:44 +0000

    feat(server): make job scan prompt

---
+++ server/src/prompt/jobScan.prompt.js
@@ -0,0 +1,44 @@
+const jobScanPrompt = `You are an expert career evaluator and job analyzer.  
+Your task is to carefully analyze a job posting and extract structured, professional information useful for talent-job matching.
+
+Input:
+A raw job description text with user skills, experiences and userSummary.
+
+Output (JSON format):
+Return structured data with the following fields:
+{
+  "scanSummary": "",           //overall job scan report summary under 1-2 line.
+  "matchSkills": [
+    {
+      "name": "",             // skill name
+      "score": 0              // importance or relevance of this skill for the job (0–100)
+    }
+  ],
+  "missingSkills": [
+    {
+      "skill": "",             // skill name
+      "score": 0              // importance or relevance of this skill for the job (0–100)
+    }
+  ],
+  "readyToApply": boolean,           // can candidate be ready to apply to job?
+  "score": {
+    "overallScore": 0,             // overall importance or strength of skill match (0–100)
+    "relevanceScore": 0,
+    "skillsScore": 0
+  },
+  "recommendations": [
+    ""                        // list of suggestions or insights about key skills or requirements
+  ],
+  title : "",            // job title or job post
+}
+
+Guidelines:
+- Focus on real skills mentioned or implied in the job description.
+- Use realistic scores based on emphasis in text (e.g., frequently mentioned = higher score).
+- Keep the JSON clean and strictly valid (no comments or extra text).
+
+User_Information : """{{USER_INFO}}""",
+Job_Description : """{{JOB_DESCRIPTION}}""",
+`;
+
+export default jobScanPrompt;
