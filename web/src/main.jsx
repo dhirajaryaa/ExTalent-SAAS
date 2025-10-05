ad07792b2a91e6b2f80cc2fbaae935fb88de0a64
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-05 23:54:22 +0000

    feat(web): router setup

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { Home, NotFound } from "./pages";
 
-createRoot(document.getElementById('root')).render(
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />, // landing page
+  },
+  {
+    element: <App />, // protected routes
+  },
+  {
+    path: "*",
+    element: <NotFound />, // 404 not found page
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={appRouter} />
+  </StrictMode>
+);
