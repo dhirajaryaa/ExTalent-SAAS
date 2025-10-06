cac9576411784ec06b3f4547cab3383ee99af6b3
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-06 13:22:27 +0000

    refactor(web): localstorage token set and error log

--- web/src/api/baseApi.js
+++ web/src/api/baseApi.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
+const token = localStorage.getItem("token");
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_API,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
   },
   withCredentials: true,
 });
-
 
 export default api;
--- web/src/lib/authChecker.js
+++ web/src/lib/authChecker.js
@@ -7,15 +7,22 @@ const authChecker = async () => {
     const authUser = await api.get("/auth/me");
     if (authUser.data?.isSuccess) {
       const loginUser = authUser.data?.data;
-      setUser(loginUser);
+      setUser(loginUser?.user);
+      localStorage.setItem("token", loginUser?.accessToken);
       return { isAuthenticated: true, user: loginUser };
     } else {
       removeUser();
+      localStorage.setItem("token", null);
       return { isAuthenticated: false, user: null };
     }
   } catch (error) {
-    console.error("Authorization Error:", error.response?.data?.message);
+    console.log(error?.response?.data?.code);
+    if (error?.response?.data?.name === "AUTHERROR") {
+      console.error("session expired! please login again.");
+    }
+    console.error("Authorization Error:", error?.response?.data?.message);
     removeUser();
+    localStorage.setItem("token", null);
     return { isAuthenticated: false, user: null };
   }
 };
