ab28611a2dd559e00c5f88d5f96d7b11a7966b68
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-08 20:53:32 +0000

    refactor(web): remove active page onClick

--- web/src/components/custom/base/NavLinks.jsx
+++ web/src/components/custom/base/NavLinks.jsx
@@ -1,33 +1,35 @@
-import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'
-import uiStore from '@/store/uiStore';
-import { Link ,useLocation} from 'react-router'
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Link, useLocation } from "react-router";
 
-function NavLinks({links}) {
-    const { pathname } = useLocation();
-    const {activePage,setActivePage} = uiStore()
+function NavLinks({ links }) {
+  const { pathname } = useLocation();
   return (
-   <SidebarMenu className={"mt-10"}>
-          {links.map((link) => (
-            <SidebarMenuItem key={link.title}>
-              <Link to={link.url} onClick={()=>setActivePage(link.title)}>
-                <SidebarMenuButton
-                  asChild
-                  tooltip={link.name}
-                  isActive={pathname.startsWith(link.url)}
-                  className="data-[active=true]:bg-primary data-[active=true]:text-background mx-auto text-sidebar-accent-foreground/60 w-[90%]"
-                >
-                  <div className="py-6 flex gap-3 text-base sm:text-[15px] px-3 font-medium ">
-                    <span>
-                      <link.icon className="size-5 sm:size-6" />
-                    </span>
-                    <span>{link.title}</span>
-                  </div>
-                </SidebarMenuButton>
-              </Link>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
-  )
+    <SidebarMenu className={"mt-10"}>
+      {links.map((link) => (
+        <SidebarMenuItem key={link.title}>
+          <Link to={link.url}>
+            <SidebarMenuButton
+              asChild
+              tooltip={link.name}
+              isActive={pathname.startsWith(link.url)}
+              className="data-[active=true]:bg-primary data-[active=true]:text-background mx-auto text-sidebar-accent-foreground/60 w-[90%]"
+            >
+              <div className="py-6 flex gap-3 text-base sm:text-[15px] px-3 font-medium ">
+                <span>
+                  <link.icon className="size-5 sm:size-6" />
+                </span>
+                <span>{link.title}</span>
+              </div>
+            </SidebarMenuButton>
+          </Link>
+        </SidebarMenuItem>
+      ))}
+    </SidebarMenu>
+  );
 }
 
-export default NavLinks
+export default NavLinks;
