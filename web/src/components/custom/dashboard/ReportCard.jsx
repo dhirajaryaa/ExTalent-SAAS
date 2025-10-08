dbc1bef35235722fd2e8888567b75c2bb4e049b6
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-08 00:00:10 +0000

    feat(web): create overview card

---
+++ web/src/components/custom/dashboard/ReportCard.jsx
@@ -0,0 +1,37 @@
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { ExternalLinkIcon } from "lucide-react"
+
+function ReportCard({title,description,value,action}) {
+  return (
+      <Card className="bg-gradient-to-b to-muted from-background w-full">
+        <CardHeader>
+          <CardDescription>{title}</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl text-primary">
+          {value > 9 ? value : `0${value}`}
+          </CardTitle>
+          <CardAction>
+            {
+              action && 
+<Button variant={'ghost'}>
+  <ExternalLinkIcon />
+</Button>
+            }
+          </CardAction>
+        </CardHeader>
+        <CardFooter className="text-muted-foreground text-sm">
+           {description}
+        </CardFooter>
+      </Card>
+  )
+}
+
+export default ReportCard
