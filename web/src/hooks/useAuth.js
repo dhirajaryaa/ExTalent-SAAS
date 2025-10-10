e72a6b7fdf4f4d5303edc52488ad636bc40a6d1f
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-10 15:02:54 +0000

    feat(web):login api create an d custom hook create

--- web/src/hooks/useAuth.js
+++ web/src/hooks/useAuth.js
@@ -1,11 +1,16 @@
-import { logoutUserAPI } from "@/api/auth.api";
-import { useMutation } from "@tanstack/react-query";
+import { loginUserAPI, logoutUserAPI } from "@/api/auth.api";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
-const useAuth = () => {
+const useAuth = (enabled=false) => {
   const logoutUser = useMutation({
     mutationFn: logoutUserAPI,
   });
-  return { logoutUser };
+  const loginUser = useQuery({
+    queryKey: ["loginUser"],
+    queryFn: loginUserAPI,
+    enabled
+  });
+  return { logoutUser, loginUser };
 };
 
 export default useAuth;
