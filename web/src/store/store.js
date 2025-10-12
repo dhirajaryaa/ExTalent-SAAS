3f2d2440b4999927e07225d3b38022e68d45d1cb
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-12 23:46:59 +0000

    feat(web): store created for save jobs and info

--- web/src/store/store.js
+++ web/src/store/store.js
@@ -2,12 +2,14 @@ import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createAuthSlice } from "./authStore";
 import { createUserSlice } from "./userSlice";
+import { createJobSlice } from "./jobStore";
 
 // create global store
 export const useStore = create(
   devtools((...args) => ({
     ...createAuthSlice(...args),
     ...createUserSlice(...args),
+    ...createJobSlice(...args),
   }))
 );
 
@@ -17,3 +19,5 @@ export const setUser = (user) => useStore.getState().setUser(user);
 export const removeUser = () => useStore.getState().removeUser();
 
 export const setProfile = (profile) => useStore.getState().setProfile(profile);
+
+export const setJobs = (jobs) => useStore.getState().setJobs(jobs);
