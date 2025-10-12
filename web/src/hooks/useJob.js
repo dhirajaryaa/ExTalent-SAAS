dd7d3f960340c613859968428d307a31de33dd81
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-12 22:48:19 +0000

    refactor(web): custom job hook enhanced and parms passout

--- web/src/hooks/useJob.js
+++ web/src/hooks/useJob.js
@@ -1,17 +1,20 @@
 import { getJobsAPI } from "@/api/job.api";
 import { useQuery } from "@tanstack/react-query";
 
-export const useJob = (jobId, enable = "false") => {
+export const useJob = (
+  prams = { pageNo: 1, limit: 10, jobId: "" },
+  enable = "false"
+) => {
   const getAllJobs = useQuery({
-    queryKey: [jobs],
-    queryFn: getJobsAPI,
-    enabled: enable
+    queryKey: ["jobs"],
+    queryFn: ()=>getJobsAPI(prams?.pageNo,prams?.limit),
+    enabled: enable,
   });
 
   const getJobWithId = useQuery({
-    queryKey: [jobs, jobId],
-    queryFn: () => getJobsAPI(jobId),
-      enabled: enable
+    queryKey: ["jobs", prams?.jobId],
+    queryFn: () => getJobsAPI(prams?.jobId),
+    enabled: enable,
   });
 
   return { getAllJobs, getJobWithId };
