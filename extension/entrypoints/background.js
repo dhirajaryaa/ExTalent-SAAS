c1d159050bb8c6de0fdeff858dd2d3fe70a88f7d
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-13 23:50:25 +0000

    feat(web): welcome page navigate on install

--- extension/entrypoints/background.js
+++ extension/entrypoints/background.js
@@ -1,3 +1,10 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+// console.log('Hello background.');
+chrome.runtime.onInstalled.addListener((details)=>{
+  if(details.reason === 'install'){
+    console.log('extension installed');
+    chrome.tabs.create({ url: 'welcome.html' });
+  }
+})
+
 });
---
+++ extension/entrypoints/welcome/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main className='w-full h-screen'>
+        <h1>Welcome</h1>
+    </main>
+  )
+}
+
+export default App
