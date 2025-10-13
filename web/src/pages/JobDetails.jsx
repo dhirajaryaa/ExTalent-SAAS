f4973a5b31d8cd099afd3fd59677a2f050071478
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-13 06:37:47 +0000

    refactor(web): emergent code and style

--- web/src/pages/JobDetails.jsx
+++ web/src/pages/JobDetails.jsx
@@ -4,9 +4,11 @@ import {
   Recommendation,
   SkillAnalysis,
 } from "@/components/custom";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Card,
+  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
@@ -26,7 +28,7 @@ function JobDetails() {
 
   if (isLoading) return <Loading />;
 
-const job = data?.data;
+  const job = data?.data;
   //   jobId: "4258062434",
   //   title: "Full Stack Developer (MERN + Next.js + Python)",
   //   companyName: "Cloudely, Inc",
@@ -110,24 +112,32 @@ const job = data?.data;
           <img
             src={job?.logo}
             alt={job?.title}
-            className="size-14 sm:size-max rounded-lg col-start-1 row-start-1"
+            className="size-14 sm:size-20 rounded-lg col-start-1 row-start-1"
           />
           <div>
-            <CardTitle className={"text-lg sm:text-xl"}>{job?.title}</CardTitle>
+            <CardTitle className={"text-lg sm:text-xl"}>{job?.title} </CardTitle>
             <CardDescription className={"flex items-center gap-2"}>
               {job?.companyName}
             </CardDescription>
-
-            <Button asChild size="sm" className={"mt-2 mx-1"}>
+            <div className="flex gap-2 items-center">
+            <Button asChild size="sm">
               <a href={job?.url} target="_blank" rel="noopener noreferrer">
                 <ExternalLink />
                 View Posting
               </a>
             </Button>
-            <Button size="sm" className={"mt-2 mx-1"} variant={"outline"}>
+            <Button size="sm" variant={"outline"}>
               <Bookmark />
               Save Job
             </Button>
+             <Badge
+              size="sm"
+              className={'ml-auto'}
+              variant={job?.readyToApply ? "default" : "destructive"}
+            >
+              {job?.readyToApply ? "Applicable" : "Not Applicable"}
+            </Badge>
+            </div>
           </div>
         </CardHeader>
         <Separator />
@@ -139,6 +149,13 @@ const job = data?.data;
             missingSkills={job?.missingSkills}
             matchSkills={job?.matchSkills}
           />
+          {/* job summary  */}
+          <div className="space-y-2">
+            <h3 className="text-base font-semibold">Job Summary</h3>
+            <p className="text-muted-foreground text-[15px]">
+              {job?.scanSummary}
+            </p>
+          </div>
         </CardContent>
         <CardFooter>
           <Recommendation recommendations={job.recommendations} />
