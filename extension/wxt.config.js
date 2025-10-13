7008f685ccfc2046217a887c32af231dfceddea0
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-13 23:06:49 +0000

    feat(ext): shadcn ui,manifest and tailwind css setup

--- extension/wxt.config.js
+++ extension/wxt.config.js
@@ -1,6 +1,35 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  alias: {
+    "@": path.resolve(__dirname, "./"),
+  },
+  manifest: ({ browser, manifestVersion, mode, command }) => {
+    return {
+      name: "ExTalent: Job score Generator",
+      short_name: "ExTalent",
+      version: "1.0.0",
+      description: "Job score Generator",
+      permissions: [
+        "activeTab",
+        "cookies",
+        "contextMenus",
+        "tabs",
+        "scripting",
+        "storage",
+      ],
+    };
+  },
+  vite: () => ({
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./"), // or "./src" if using src directory
+      },
+    },
+  }),
 });
