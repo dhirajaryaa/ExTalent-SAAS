68b9a530c2cd56b7d28f79ca26e8f64b73b1ee6c
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-21 19:18:54 +0000

    feat(ext): job information like id,title,description,company,logo etc selected

--- extension/entrypoints/jobScan.content/index.js
+++ extension/entrypoints/jobScan.content/index.js
@@ -6,11 +6,33 @@ export default defineContentScript({
     await browser.runtime.onMessage.addListener(
       async (message, sender, sendResponse) => {
         if (message.action === "SCAN_JOB_WITH_EXTALENT") {
-          // select job information 
+          // select job information
+          const jobId = new URLSearchParams(window.location.search).get("currentJobId");
+          const jobInfo = document.querySelector(
+            "div.job-details-jobs-unified-top-card__job-title h1"
+          );
+          const jobTitle = jobInfo?.innerText;
+          const jobUrl = jobInfo?.querySelector("a")?.href;
+          const jobCompany = document
+            .querySelector(
+              "div.jobs-company__box a[data-view-name='job-details-about-company-name-link']"
+            )
+            ?.innerText.trim();
+          const companyLogo = document.querySelector(
+            "div.jobs-company__box img[alt*='company logo']"
+          )?.src;
+
           const jobDescription = document
             .querySelector("div#job-details p")
             ?.innerText.replace(/\n/g, "")
             .trim();
+
+          console.log("jobId", jobId);
+          console.log("job title", jobTitle);
+          console.log("job url", jobUrl);
+          console.log("job company", jobCompany);
+          console.log("company logo", companyLogo);
+          console.log("job description", jobDescription);
         }
       }
     );
