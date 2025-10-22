516ac1d97c9b9eb6aa2689f33f2cb05eaeb66583
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-22 23:34:00 +0000

    feat(ext): mount linkedin page ui using shadow dom ui

--- extension/entrypoints/jobScan.content/index.jsx
+++ extension/entrypoints/jobScan.content/index.jsx
@@ -1,12 +1,20 @@
 import { browser } from "#imports";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../../assets/index.css";
+import App from "./App";
+
+let uiRef = null;
 
 export default defineContentScript({
   matches: ["https://www.linkedin.com/jobs/*"],
+  cssInjectionMode: "ui",
+  runAt: "document_end",
   async main(ctx) {
+    // check message and select job information
     await browser.runtime.onMessage.addListener(
       async (message, sender, sendResponse) => {
         if (message.action === "EXT_EXTRACT_JOB_INFO") {
-
           // select job information
           const jobId = new URLSearchParams(window.location.search).get(
             "currentJobId"
@@ -62,5 +70,32 @@ export default defineContentScript({
         }
       }
     );
+
+    // Inject UI on job page
+    uiRef = await mountShadowUI(ctx);
+    uiRef.mount();
   },
 });
+
+async function mountShadowUI(ctx){
+  return await createShadowRootUi(ctx, {
+    name: "extalent-ui",
+    position: "inline",
+    anchor: ".job-details-jobs-unified-top-card__container--two-pane",
+    append: "after",
+    onMount: (uiContainer, shadow, shadowContainer) => {
+      const app = document.createElement("div");
+      uiContainer.append(app);
+      const root = ReactDOM.createRoot(app);
+      root.render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      );
+      return root;
+    },
+    onRemove: (root) => {
+      root?.unmount();
+    },
+  });
+};
