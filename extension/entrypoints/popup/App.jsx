d889afaa58f17ad14dd900da23dd3507f8f0bdd5
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-24 02:14:30 +0000

    feat(ext): add sign in btn in popup ui

--- extension/entrypoints/popup/App.jsx
+++ extension/entrypoints/popup/App.jsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {
-  getLocalStorage,
-  getSyncStorage,
-  setSyncStorage,
-} from "@/utils/extStorage";
+import { getLocalStorage, setSyncStorage } from "@/utils/extStorage";
 import Logo from "@/components/custom/Logo";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import ListItem from "./components/ListItem";
 import Logout from "./components/Logout";
-import { ExternalLink, UserCircle } from "lucide-react";
+import { ExternalLink, UserCircle,Github } from "lucide-react";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -18,8 +14,6 @@ function App() {
   useEffect(() => {
     const getLocalUser = async () => {
       const user = await getLocalStorage("user");
-      const linkedinIngrateSetting = await getSyncStorage("ingrate");
-      !linkedinIngrateSetting && (await setSyncStorage("ingrate", true));
       if (user) {
         setUser(user);
       }
@@ -28,78 +22,100 @@ function App() {
   }, []);
 
   const toggleLinkedinIntegration = async () => {
-    if(linkedinIngrate){
+    if (linkedinIngrate) {
       await setSyncStorage("ingrate", !linkedinIngrate);
       setLinkedinIngrate(!linkedinIngrate);
-    }else{
-       await setSyncStorage("ingrate", !linkedinIngrate);
+    } else {
+      await setSyncStorage("ingrate", !linkedinIngrate);
       setLinkedinIngrate(!linkedinIngrate);
     }
   };
 
+  async function signInWithGithub() {
+    const oauthLink = `${
+      import.meta.env.VITE_GITHUB_LOGIN_URL
+    }?source=extension`;
+    // open login page
+    window.open(oauthLink, "_blank");
+  }
+
   return (
     <div className=" bg-background text-background-foreground w-[350px]">
-      <header className="w-full border-b-1 py-2 px-3">
-        <Logo variant="sm" />
-      </header>
-      {/* user avatar  */}
-      <div className="relative mt-2 flex items-center justify-center flex-col gap-1">
-        {/* logout btn  */}
-        <Logout />
-        {/* user info  */}
-        <Avatar className={"size-24 border-3 border-primary p-[2px]"}>
-          <AvatarImage
-            src={user?.avatar}
-            alt={user?.name}
-            className={"rounded-full object-contain"}
-          />
-          <AvatarFallback>{user?.name?.split(" ")[0]}</AvatarFallback>
-        </Avatar>
-        <h2 className="text-xl font-semibold capitalize">{user?.name}</h2>
-        <Button variant={"link"} className={"h-0 cursor-pointer"} asChild>
-          <a href={user?.resume?.url} target="_blank" rel="noreferrer">
-            View Resume
-          </a>
-        </Button>
-      </div>
-      <div className="p-2 px-4 space-y-2">
-        {/* linkedin integrate on/off  */}
-        <ListItem
-          title="Integrate on LinkedIn"
-          description=" Enable LinkedIn integration for interact with linkedin pages."
-          label="linkedin-integrate"
-          enabled={linkedinIngrate}
-          action={toggleLinkedinIntegration}
-        />
-        {/* dark mode   */}
-        <ListItem
-          title="Dark Mode"
-          description="toggle between dark and light mode theme"
-          label="dark-mode"
-          enabled={darkMode}
-          action={() => {
-            setDarkMode(!darkMode);
-          }}
-        />
-        <div className="space-y-2 w-full mt-3">
-          <Button className={"w-full"} asChild>
-            <a
-              href={`${import.meta.env.VITE_DASHBOARD_LINK}/dashboard`}
-              target="_blank"
-            >
-              <ExternalLink /> View Dashboard
-            </a>
-          </Button>
-          <Button className={"w-full"} asChild variant={"outline"}>
-            <a
-              href={`${import.meta.env.VITE_DASHBOARD_LINK}/profile`}
-              target="_blank"
-            >
-              <UserCircle /> Manage Profile
-            </a>
+      {user ? (
+        <>
+          {/* header  */}
+          <header className="w-full border-b-1 py-2 px-3">
+            <Logo variant="sm" />
+          </header>
+          <div className="relative mt-2 flex items-center justify-center flex-col gap-1">
+            {/* logout btn  */}
+            <Logout />
+            {/* user info  */}
+            <Avatar className={"size-24 border-3 border-primary p-[2px]"}>
+              <AvatarImage
+                src={user?.avatar}
+                alt={user?.name}
+                className={"rounded-full object-contain"}
+              />
+              <AvatarFallback>{user?.name?.split(" ")[0]}</AvatarFallback>
+            </Avatar>
+            <h2 className="text-xl font-semibold capitalize">{user?.name}</h2>
+            <Button variant={"link"} className={"h-0 cursor-pointer"} asChild>
+              <a href={user?.resume?.url} target="_blank" rel="noreferrer">
+                View Resume
+              </a>
+            </Button>
+          </div>
+          <div className="p-2 px-4 space-y-2">
+            {/* linkedin integrate on/off  */}
+            <ListItem
+              title="Integrate on LinkedIn"
+              description=" Enable LinkedIn integration for interact with linkedin pages."
+              label="linkedin-integrate"
+              enabled={linkedinIngrate}
+              action={toggleLinkedinIntegration}
+            />
+            {/* dark mode   */}
+            <ListItem
+              title="Dark Mode"
+              description="toggle between dark and light mode theme"
+              label="dark-mode"
+              enabled={darkMode}
+              action={() => {
+                setDarkMode(!darkMode);
+              }}
+            />
+            <div className="space-y-2 w-full mt-3">
+              <Button className={"w-full"} asChild>
+                <a
+                  href={`${import.meta.env.VITE_DASHBOARD_LINK}/dashboard`}
+                  target="_blank"
+                >
+                  <ExternalLink /> View Dashboard
+                </a>
+              </Button>
+              <Button className={"w-full"} asChild variant={"outline"}>
+                <a
+                  href={`${import.meta.env.VITE_DASHBOARD_LINK}/profile`}
+                  target="_blank"
+                >
+                  <UserCircle /> Manage Profile
+                </a>
+              </Button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="w-full h-50 flex items-center justify-center">
+          <Button
+            size="lg"
+            className={"sm:text-base"}
+            onClick={signInWithGithub}
+          >
+            <Github className="sm:size-5" fill="currentColor" /> Sign in Github
           </Button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
